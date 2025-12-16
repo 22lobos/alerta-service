0d04d32272d212f5aab29a3ab53d27a107dbf0d0
22lobos <cr.loboso@duocuc.cl>
2025-12-15 23:01:47 -0300

    Microservicio alerta-service funcional

--- src/main/java/com/example/alerta_service/AlertaServiceApplication.java
+++ src/main/java/com/example/alerta_service/AlertaServiceApplication.java
@@ -2,9 +2,14 @@ package com.example.alerta_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.alerta_service")
+@EnableJpaRepositories(basePackages = "com.example.alerta_service.repository")
+@EntityScan(basePackages = "com.example.alerta_service.model")
 public class AlertaServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AlertaServiceApplication.class, args);
     }
