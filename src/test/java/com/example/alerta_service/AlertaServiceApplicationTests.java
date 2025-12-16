5f7161fa786b4639d22f748ceae19a5ed090ce46
22lobos <cr.loboso@duocuc.cl>
2025-12-15 21:33:43 -0300

    first commit

---
+++ src/test/java/com/example/alerta_service/AlertaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alerta_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
